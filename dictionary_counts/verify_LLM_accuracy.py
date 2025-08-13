4c89aad90252fcafa1b027dc014d8167a3ed2060
ryanbai2008 <69367510+ryanbai2008@users.noreply.github.com>
2025-08-12 20:50:33 -0700

    Update verify_LLM_accuracy.py
    updated to be able to verify for stepwise + relational graphs instead of just baseline, hierarchical, and stepwise. Just change the stepwise_relational boolean value

--- dictionary_counts/verify_LLM_accuracy.py
+++ dictionary_counts/verify_LLM_accuracy.py
@@ -1,10 +1,14 @@
 import json
 
-#may need to change file name which our double checked values are saved into
-OUTPUT_FILE_NAME = "hierarchical_gemini.json"
+#stepwise + relational graphs requires different code to check for second-best classifications, set this to true
+#if double checking stepwise + relational graphs
+stepwise_relational = True
 
-#lines with these phrases will be the only lines displayed, also the lines will be displayed one by one, not all at a time, so it is easier to manually double check
-detect_phrases = ["Gemini classification:"]
+#file name which our double chec=ked values are saved into
+OUTPUT_FILE_NAME = "relational_graphs_gemini.json"
+
+#lines with these phrases will be 
+detect_phrases = ["Gemini with KB + Prolog:"]
 
 with open(r"google doc file.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
@@ -14,14 +18,18 @@ with open(r"ground_truths.txt", 'r', encoding='utf-8') as file:
     ground_truths = file.readlines()
 ground_truths = [ground_truth.strip().lower() for ground_truth in ground_truths]
 
-#i think these are the ones missing the fallacy example statements, so skip these
+#i think these are the ones missing the fallacy example statemtns, so skip these
 ground_truths.remove("methodology")
 ground_truths.remove("reasoning")
 
 ground_truth_idx = 0
 accuracy_dict = {}
+if stepwise_relational:
+    second_best_dict = {}
 for ground_truth in ground_truths:
     accuracy_dict[ground_truth] = 0
+    if stepwise_relational:
+        second_best_dict[ground_truth] = 0
 
 tot_correct = 0
 
@@ -33,10 +41,14 @@ for line in lines:
             if truth == '1':
                 tot_correct += 1
                 accuracy_dict[ground_truths[ground_truth_idx]] = 1
+            if truth == '3' and stepwise_relational:
+                second_best_dict[ground_truths[ground_truth_idx]] = 1
             ground_truth_idx += 1
         
 #do sanity check to make sure no big errors
 print(tot_correct)
 
+if stepwise_relational:
+    accuracy_dict = [accuracy_dict, second_best_dict]
 with open(OUTPUT_FILE_NAME, 'w') as file:
     json.dump(accuracy_dict, file, indent=1)
